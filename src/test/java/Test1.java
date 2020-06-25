92dc552209a46ad27be2aaa1208643a05f5c2b32
Benjamin Kidd <benkidd361@gmail.com>
2020-06-25 14:54:27 -0400

    init

---
+++ src/test/java/Test1.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class Test1 {
+    @Test
+    public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
+
+        given().
+                when().
+                get("http://ergast.com/api/f1/2017/circuits.json").
+                then().
+                assertThat().
+                body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
+    }
+}
